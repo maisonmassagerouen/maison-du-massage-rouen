f37f3aea413446ee13e9def9b5f76552003a3532
maisonmassagerouen <maisonmassagerouen@gmail.com>
2026-09-24 00:53:35 +0200

    maj path "base: '/maison-du-massage-rouen/',"

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  base: '/maison-du-massage-rouen/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
